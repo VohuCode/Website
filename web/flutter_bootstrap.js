dc8e85b35ad03239f068a12817b4a11d04259077
vohucode <vohucode@gmail.com>
2025-05-27 18:27:13 +0330

    fixing ux in courses and blog page and optimizing whole project

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "672322444"
+    serviceWorkerVersion: "3139076027"
   }
 });
